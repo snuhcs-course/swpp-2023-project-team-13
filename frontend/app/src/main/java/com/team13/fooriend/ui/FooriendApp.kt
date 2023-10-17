1c63e6f3403862729bc0da595953ad7d78e66e50
mechanicjo <jych1109@gmail.com>
2023-10-17 17:56:11 +0900

    navigation 계층화
    root navigation을 만들어서 navigation을 계층화 했습니다.
    그런데 Navigation Bar에서 터지네요..

--- frontend/app/src/main/java/com/team13/fooriend/ui/FooriendApp.kt
+++ frontend/app/src/main/java/com/team13/fooriend/ui/FooriendApp.kt
@@ -2,38 +2,14 @@ package com.team13.fooriend.ui
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.team13.fooriend.ui.component.BottomBar
-import com.team13.fooriend.ui.screen.login.LogInScreen
-import com.team13.fooriend.ui.screen.signup.SignUpScreen
+import com.team13.fooriend.core.graph.RootNavigationGraph
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FooriendApp() {
     val navController = rememberNavController()
-    //FooriendNavHost(navController = navController)
-    BottomBar(navController = navController)
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun FooriendNavHost(navController: NavHostController) {
-    NavHost(
-        navController = navController,
-        startDestination = "login"
-    ) {
-        composable("login") {
-            LogInScreen()
-        }
-        composable("signup") {
-            SignUpScreen()
-        }
-        composable("home"){
-
-        }
-    }
+    //BottomBar(navController = navController)
+    RootNavigationGraph(navController = navController)
 }
--- frontend/app/src/main/java/com/team13/fooriend/ui/component/BottomBar.kt
+++ frontend/app/src/main/java/com/team13/fooriend/ui/component/BottomBar.kt
@@ -1,23 +1,56 @@
 package com.team13.fooriend.ui.component
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavHostController
-import com.team13.fooriend.ui.navigation.BottomNavigation
-import com.team13.fooriend.ui.navigation.NavigationGraph
+import com.team13.fooriend.ui.navigation.BottomNavItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BottomBar(navController: NavHostController){
-    Scaffold(
-        bottomBar = { BottomNavigation(navController = navController) }
-    ) {
-        Box(Modifier.padding(it)){
-            NavigationGraph(navController = navController)
+fun BottomBar(navController: NavHostController) {
+    val items = listOf<BottomNavItem>(
+        BottomNavItem.Home,
+        BottomNavItem.Social,
+        BottomNavItem.MyPage,
+    )
+    var selectedItem by remember { mutableStateOf(0)}
+    var currentRoute by remember { mutableStateOf(BottomNavItem.Home.route)}
+
+    items.forEachIndexed{ index, navigationItem ->
+        if(navigationItem.route == currentRoute){
+            selectedItem = index
+        }
+    }
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                alwaysShowLabel = true,
+                icon = { Icon(item.icon!!, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = selectedItem == index,
+                onClick = {
+                    selectedItem = index
+                    currentRoute = item.route
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
         }
     }
 }
