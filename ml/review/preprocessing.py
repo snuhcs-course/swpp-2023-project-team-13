1abd048dd75881648f66364a485a1405e9c779ab
eldpswp99 <nrg1392@naver.com>
2023-11-02 18:38:46 +0900

    fix typo

--- ml/review/preprocessing.py
+++ ml/review/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def preprcessing(data):
+def preprocessing(data):
     data_list = []
 
     for q, label in zip(data["review"], data["y"]):
