cd13fbfb09f4e0bc05bf5c757784391833606f63
mechanicjo <jych1109@gmail.com>
2023-11-21 22:16:09 +0900

    test: BottomBar navigation testing
    BottomBar Component에 대한 test 코드입니다.
    -처음 앱 시작시 HomeScreen에서 시작
    -Social Icon 클릭시 Social Screen으로 이동
    -MyPage Icon 클릭시 MyPage Screen으로 이동
    -Home Icon 클릭시  Home Screen으로 이동
    
    TODO: 위치 정보 공유 허용을 테스트 시작하기 전에 일일히 에뮬레이터 실행해서 허용해 주어야 합니다. 이건 추후 test 전에 setup 코드 작성으로 해결할 예정입니다.

--- frontend/app/src/androidTest/java/com/team13/fooriend/BottomBarTest.kt
+++ frontend/app/src/androidTest/java/com/team13/fooriend/BottomBarTest.kt
@@ -1,5 +1,11 @@
 package com.team13.fooriend
 
+import android.content.Context
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertIsFocused
@@ -18,6 +24,7 @@ import com.team13.fooriend.core.graph.RootNavigationGraph
 import com.team13.fooriend.ui.component.BottomBar
 import com.team13.fooriend.ui.component.BottomNavigation
 import com.team13.fooriend.ui.navigation.BottomNavItem
+import com.team13.fooriend.ui.util.checkForPermission
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
@@ -26,15 +33,20 @@ import org.junit.Test
 class BottomBarTest {
     @get:Rule
     val composeTestRule = createComposeRule()
-    lateinit var navController: TestNavHostController
+    private lateinit var navController: TestNavHostController
+    lateinit var context: Context
 
     @Before
     fun setUpNavHost(){
+        // 위치 정보 공유 허가 코드 작성 필요
         composeTestRule.setContent {
             navController = TestNavHostController(LocalContext.current)
             navController.navigatorProvider.addNavigator(ComposeNavigator())
-            BottomBar(navController = navController, context = LocalContext.current, showBottomBar = true)
+            context = LocalContext.current
+            BottomBar(navController = navController, context = context, showBottomBar = true)
         }
+        val route = navController.currentBackStackEntry?.destination?.route
+        Log.d("BottomBarTestLog", route.toString())
     }
 
     @Test
@@ -49,28 +61,147 @@ class BottomBarTest {
             .onNodeWithText("MyPage")
             .assertIsNotSelected()
     }
-    // 이후부터는 스마트폰 필요
     @Test
-    fun performClick_OnSocialIcon_navigatesToSocialScreen(){
+    fun performClick_OnSocialIcon_navigatesToSocialScreen_startHomeScreen(){
+        // Home -> Social
+        composeTestRule
+            .onNodeWithText("Home")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithText("Social")
+            .assertIsNotSelected()
+            .performClick()
+        val route = navController.currentBackStackEntry?.destination?.route
+        Assert.assertEquals(route, BottomNavItem.Social.route)
+        composeTestRule
+            .onNodeWithText("Home")
+            .assertIsNotSelected()
+        composeTestRule
+            .onNodeWithText("Social")
+            .assertIsSelected()
+        composeTestRule
+            .onNodeWithText("MyPage")
+            .assertIsNotSelected()
+    }
+
+    @Test
+    fun performClick_OnSocialIcon_navigatesToSocialScreen_startMyPageScreen(){
+        // MyPage -> Social
+        composeTestRule
+            .onNodeWithText("MyPage")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithText("Social")
+            .assertIsNotSelected()
+            .performClick()
+        val route = navController.currentBackStackEntry?.destination?.route
+        Assert.assertEquals(route, BottomNavItem.Social.route)
+        composeTestRule
+            .onNodeWithText("Home")
+            .assertIsNotSelected()
+        composeTestRule
+            .onNodeWithText("Social")
+            .assertIsSelected()
+        composeTestRule
+            .onNodeWithText("MyPage")
+            .assertIsNotSelected()
+    }
+
+    @Test
+    fun performClick_OnMyPageIcon_navigatesToMyPageScreen_startHomeScreen(){
+        // Home -> MyPage
+        composeTestRule
+            .onNodeWithText("Home")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithText("MyPage")
+            .assertIsNotSelected()
+            .performClick()
+        val route = navController.currentBackStackEntry?.destination?.route
+        Assert.assertEquals(route, BottomNavItem.MyPage.route)
+        composeTestRule
+            .onNodeWithText("Home")
+            .assertIsNotSelected()
+        composeTestRule
+            .onNodeWithText("Social")
+            .assertIsNotSelected()
+        composeTestRule
+            .onNodeWithText("MyPage")
+            .assertIsSelected()
+    }
+
+    @Test
+    fun performClick_OnMyPageIcon_navigatesToMyPageScreen_startSocialScreen(){
+        // Social -> MyPage
         composeTestRule
             .onNodeWithText("Social")
             .performClick()
+
+        composeTestRule
+            .onNodeWithText("MyPage")
+            .assertIsNotSelected()
+            .performClick()
+        val route = navController.currentBackStackEntry?.destination?.route
+        Assert.assertEquals(route, BottomNavItem.MyPage.route)
+        composeTestRule
+            .onNodeWithText("Home")
+            .assertIsNotSelected()
+        composeTestRule
+            .onNodeWithText("Social")
+            .assertIsNotSelected()
+        composeTestRule
+            .onNodeWithText("MyPage")
+            .assertIsSelected()
     }
 
     @Test
-    fun performClick_OnMyPageIcon_navigatesToMyPageScreen(){
+    fun performClick_OnHomeIcon_navigatesHomeScreen_startSocialScreen(){
+        // Social -> Home
+        composeTestRule
+            .onNodeWithText("Social")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithText("Home")
+            .assertIsNotSelected()
+            .performClick()
+        val route = navController.currentBackStackEntry?.destination?.route
+        Assert.assertEquals(route, BottomNavItem.Home.route)
+        composeTestRule
+            .onNodeWithText("Home")
+            .assertIsSelected()
+        composeTestRule
+            .onNodeWithText("Social")
+            .assertIsNotSelected()
         composeTestRule
             .onNodeWithText("MyPage")
             .assertIsNotSelected()
     }
 
     @Test
-    fun test3(){
+    fun performClick_OnHomeIcon_navigatesHomeScreen_startMyPageScreen(){
+        // MyPage -> Home
+        composeTestRule
+            .onNodeWithText("MyPage")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithText("Home")
+            .assertIsNotSelected()
+            .performClick()
+        val route = navController.currentBackStackEntry?.destination?.route
+        Assert.assertEquals(route, BottomNavItem.Home.route)
+        composeTestRule
+            .onNodeWithText("Home")
+            .assertIsSelected()
+        composeTestRule
+            .onNodeWithText("Social")
+            .assertIsNotSelected()
         composeTestRule
             .onNodeWithText("MyPage")
             .assertIsNotSelected()
     }
-
-
-
 }
